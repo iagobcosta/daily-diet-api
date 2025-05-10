3d87577f9a75e5af4acd76e0b100f12a28d0cac9
Iago Barbosa <iago.barbosa@somapay.com.br>
2025-05-09 23:08:36 -0300

    chore: add config

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,14 +4,16 @@ import { app } from "../src/app" // já vamos criar esse arquivo
 import { db } from "../src/db/database"
 
 beforeAll(async () => {
+  console.log("⚙️ beforeAll iniciado")
   await app.ready()
+  console.log("✅ beforeAll finalizado")
 })
 
-// beforeEach(async () => {
-//   execSync("npm run rollback && npm run migrate")
-// })
+beforeEach(async () => {
+  execSync("npm run rollback && npm run migrate")
+})
 
 afterAll(async () => {
   await app.close()
-  // await db.destroy()
+  await db.destroy()
 })
