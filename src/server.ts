cb44899e89496e173c3e9bfd4bd027256b9d4385
Iago Barbosa <iago.barbosa@somapay.com.br>
2025-05-13 06:31:20 -0300

    chore: add host connect

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app"
 import { env } from "./env"
 
-app.listen({ port: Number(env.PORT) || 3333 }).then(() => {
+app.listen({ port: Number(env.PORT) || 3333, host: "0.0.0.0" }).then(() => {
   console.log(`🚀 HTTP server running! at port ${Number(env.PORT) || 3333}`)
 })
